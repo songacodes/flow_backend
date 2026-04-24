45beb6a32529b32af139640f95a0f7dc1df9d091
Arnaurd-Briant <songabriant@gmail.com>
2026-04-24 08:40:08 +0200

    f

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false, // Set to true if you want to see SQL queries in the console
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  logging: false,
+});
 
 const connectDB = async () => {
   try {
